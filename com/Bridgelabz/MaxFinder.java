358c203829e1f4579dca1c143db9729509400dea
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-12 23:38:09 +0530

    jUNIT Testing1

--- com/Bridgelabz/MaxFinder.java
+++ com/Bridgelabz/MaxFinder.java
@@ -1,40 +1,21 @@
 package com.Bridgelabz;
 
-import java.util.Comparator;
-
-public class MaxFinder <T extends Comparable<T>> {
-
-	T num1;
-	T num2;
-	T num3;
-	
-	 public static void main(String[] args) {
-   	  System.out.println("Maximum Integer by creating Generic class :" + new MaxFinder(2, 4, 6).GenericMax());
-   	  System.out.println("Maximum Float by creating Generic class :" + new MaxFinder(3.4f, 4.6f, 6.7f).GenericMax());
-   	  System.out.println("Maximum string by creating Generic class :" + new MaxFinder("apple","orange","banana").GenericMax());
-   }
-	
-    public  MaxFinder(T num1, T num2, T num3) {
-		super();
-		this.num1 = num1;
-		this.num2 = num2;
-		this.num3 = num3;
+public class MaxFinder {
+	public static void main(String[] args) {
+		Integer a=2; 
+		Integer b=4; 
+		Integer c=6;
+		findMaximum(a,b,c);
 	}
-
-
-    public T GenericMax() {
-    	return Maximum.GenericMax(num1, num2, num3);
-    }
-
-    
-    public static <T extends Comparable<T>> T GenericMax(T num1, T num2, T num3) {
-        T max = num1;
-        if (num2.compareTo(max) > 0)
-            max = num2;
-        if (num3.compareTo(max) > 0)
-            max = num3;
-        return max;
-    }
-
-    }
-
+		
+		private static void findMaximum(Integer a, Integer b, Integer c) {
+			Integer max = a;
+			if (b.compareTo(a)>0) {
+				max = b;
+		}
+			if (c.compareTo(max)>0) {
+				max = c;
+			}
+		 System.out.println("Maximum of numbers are " +max);
+		}
+}
