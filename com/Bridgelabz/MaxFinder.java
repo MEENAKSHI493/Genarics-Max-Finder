a3c6a91d1c7c0cc3c3d94d09adcabad7a781e575
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-07-12 22:06:53 +0530

    Refactor 2

--- com/Bridgelabz/MaxFinder.java
+++ com/Bridgelabz/MaxFinder.java
@@ -1,10 +1,40 @@
 package com.Bridgelabz;
 
-public class MaxFinder {
+import java.util.Comparator;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class MaxFinder <T extends Comparable<T>> {
 
+	T num1;
+	T num2;
+	T num3;
+	
+	 public static void main(String[] args) {
+   	  System.out.println("Maximum Integer by creating Generic class :" + new MaxFinder(2, 4, 6).GenericMax());
+   	  System.out.println("Maximum Float by creating Generic class :" + new MaxFinder(3.4f, 4.6f, 6.7f).GenericMax());
+   	  System.out.println("Maximum string by creating Generic class :" + new MaxFinder("apple","orange","banana").GenericMax());
+   }
+	
+    public  MaxFinder(T num1, T num2, T num3) {
+		super();
+		this.num1 = num1;
+		this.num2 = num2;
+		this.num3 = num3;
 	}
 
-}
+
+    public T GenericMax() {
+    	return Maximum.GenericMax(num1, num2, num3);
+    }
+
+    
+    public static <T extends Comparable<T>> T GenericMax(T num1, T num2, T num3) {
+        T max = num1;
+        if (num2.compareTo(max) > 0)
+            max = num2;
+        if (num3.compareTo(max) > 0)
+            max = num3;
+        return max;
+    }
+
+    }
+
